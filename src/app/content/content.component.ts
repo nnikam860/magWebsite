614a79f1c499a87438ae8b5341a055836896ae0b
RobinKuntal01 <107622477+RobinKuntal01@users.noreply.github.com>
2024-04-09 13:16:20 +0000

    Commit

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ApiServiceService } from '../services/api-service.service';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
+  articles: any;
+  articleID: any;
 
+  constructor(
+    private apiServices: ApiServiceService,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.apiServices.getArticles().subscribe((data: any) => {
+      this.articles = data.allArticles;
+      
+    });
+  }
+
+  deleteArticle(articleID: number) {
+    return this.apiServices.destroyArticle(articleID).subscribe((data: any) => {
+      this.ngOnInit();
+      console.log(articleID);
+    });
+  }
+  
 }
