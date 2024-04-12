50b15b16077a3913e8953278e13c8f2122ff6ad6
RobinKuntal01 <107622477+RobinKuntal01@users.noreply.github.com>
2024-04-12 18:58:23 +0000

    cors

--- mag-backend/src/app.js
+++ mag-backend/src/app.js
@@ -5,9 +5,12 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin:process.env.CORS_ORIGIN ,
-    credentials: true
-}))
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
