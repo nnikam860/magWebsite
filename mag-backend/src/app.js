c641f2fa6d59f9a52ef43ef421c564d0043b076c
RobinKuntal01 <107622477+RobinKuntal01@users.noreply.github.com>
2024-04-12 19:00:19 +0000

    cors

--- mag-backend/src/app.js
+++ mag-backend/src/app.js
@@ -11,6 +11,12 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://fempreneurmagazine.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
