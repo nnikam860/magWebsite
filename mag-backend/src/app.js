2ca16d871d89762ffe8b9a8192ece47d50ff66d7
RobinKuntal01 <107622477+RobinKuntal01@users.noreply.github.com>
2024-05-13 16:08:39 +0000

    body parser

--- mag-backend/src/app.js
+++ mag-backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+
 
 const app = express()
 
@@ -13,7 +15,7 @@ app.use(cors({
 
 
 
-
+app.use(bodyParser.json({ limit: '10mb' }));
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended: true, limit:"16kb"}))
 app.use(express.static("public"))
