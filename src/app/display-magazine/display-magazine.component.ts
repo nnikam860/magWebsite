257b1be6e4cbe1d1546323241f8606df944fc88d
ntech1353 <92263767+ntech1353@users.noreply.github.com>
2024-05-22 12:34:13 +0000

    Content Changes

--- src/app/display-magazine/display-magazine.component.ts
+++ src/app/display-magazine/display-magazine.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild , ElementRef} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ApiServiceService } from '../services/api-service.service';
 import { ReadMoreComponent } from '../read-more/read-more.component';
 import { HttpClient } from '@angular/common/http';
@@ -8,29 +8,42 @@ import { MagServiceService } from '../services/mag-service.service';
 import { CommonModule } from '@angular/common';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+interface Magazine {
+  _id: number;
+  coverImage: string;
+  title: string;
+  createdAt: string;
+  // Add other properties as needed
+}
 
 @Component({
   selector: 'app-display-magazine',
   standalone: true,
-  imports: [CommonModule, NgxExtendedPdfViewerModule],
+  imports: [CommonModule, NgxExtendedPdfViewerModule, MatPaginatorModule],
   templateUrl: './display-magazine.component.html',
   styleUrls: ['./display-magazine.component.css']
 })
 export class DisplayMagazineComponent implements OnInit {
-  magazine: any;
+  magazine: Magazine[] = [];
+ // Specify the type as an array of Magazine
   magazineID: any;
   readMagazine: any;
   pdfUrl = '';
   isAdminLoggedIn: boolean = false;
+  page: number = 1;
+
   constructor(
     private magService: MagServiceService,
     private apiServices: ApiServiceService,
     private http: HttpClient,
     private authService: AuthService,
     private router: Router,
-  private modalservice:NgbModal) { }
+    private modalservice: NgbModal
+  ) { }
 
-@ViewChild('content') popupview!:ElementRef;
+  @ViewChild('content') popupview!: ElementRef;
 
   ngOnInit(): void {
     this.magService.getMagazines().subscribe((data: any) => {
@@ -40,6 +53,7 @@ export class DisplayMagazineComponent implements OnInit {
       this.isAdminLoggedIn = isUserLoggedIn;
     });
   }
+
   readMore(magazineID: number) {
     this.magService.getMagazineById(magazineID).subscribe((data: any) => {
       this.readMagazine = data.readMoreMagazine
@@ -47,7 +61,7 @@ export class DisplayMagazineComponent implements OnInit {
       let url = this.readMagazine.magazinePDF
       this.pdfUrl = url
       console.log(this.pdfUrl);
-      this.modalservice.open(this.popupview, {size:'lg'})
+      this.modalservice.open(this.popupview, { size: 'lg' })
     });
   }
 
@@ -59,4 +73,3 @@ export class DisplayMagazineComponent implements OnInit {
     alert('Article deleted successfully!');
   }
 }
-
